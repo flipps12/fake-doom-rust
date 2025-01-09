dd65b66dfd8b2f17364cc164c48bb32a1ab16ddb
Flipps12 <paginasypruebas12@gmail.com>
2025-01-08 22:11:20 -0300

    Menu

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,9 @@ use macroquad::prelude::*;
 mod entity;
 
 use entity::{ Player, Entity };
-use miniquad::window::set_window_size;
 
 struct RenderObjects<'a> {
     distance: f32,
-    wall: bool,
     render_wall: Option<RenderWalls<'a>>,
     render_entity: Option<Box<RenderEntity<'a>>>,
 }
@@ -38,13 +36,11 @@ struct RenderEntity<'a> {
 impl<'a> RenderObjects<'a> {
     fn new(
         distance: f32,
-        wall: bool,
         render_wall: Option<RenderWalls<'a>>,
         render_entity: Option<Box<RenderEntity<'a>>>
     ) -> Self {
         RenderObjects {
             distance,
-            wall,
             render_wall,
             render_entity,
         }
@@ -64,11 +60,11 @@ pub const MAP: [[i32; 20]; 20] = [
     [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1],
     [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1],
     [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1],
     [1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -77,9 +73,9 @@ pub const MAP: [[i32; 20]; 20] = [
 #[macroquad::main("Fake doom")]
 async fn main() {
     // Window settings
-    //set_fullscreen(true);
-    show_mouse(false);
-    set_cursor_grab(true);
+    // set_fullscreen(true);
+    set_cursor_grab(false);
+    show_mouse(true);
 
     // Load textures
     let texture_pared = load_texture("assets/pared.png").await.unwrap();
@@ -106,222 +102,246 @@ async fn main() {
         Entity { x: 8.0, y: 2.0, angle: 1.0, texture: entity_texture.clone() }
     ];
 
+    let mut menu = true;
+
     loop {
         // Reset
         clear_background(BLACK);
 
-        entities[0].move_entity(player.x, player.y);
-
         let screen_width = screen_width();
         let screen_height = screen_height();
 
-        draw_texture_ex(
-            &texture_techo,
-            0.0,
-            0.0,
-            WHITE, // Color base
-            DrawTextureParams {
-                dest_size: Some(Vec2::new(screen_width, screen_height / 2.0)), // Escalar textura a la pared
-                ..Default::default()
+        if menu {
+            let text = "Fake doom";
+            let font_size = 40;
+
+            // Medir el texto
+            let text_dimensions = measure_text(text, None, font_size, 1.0);
+    
+            // Coordenadas centradas
+            let text_x = (screen_width - text_dimensions.width) / 2.0;
+            draw_text(
+                &format!("{}", text),
+                text_x,
+                screen_height / 4.0,
+                font_size as f32,
+                WHITE
+            );
+
+            if is_key_down(KeyCode::Enter) {
+                show_mouse(false);
+                set_cursor_grab(true);
+                menu = false;
             }
-        );
+        } else {
+            entities[0].move_entity(player.x, player.y);
 
-        draw_texture_ex(
-            &texture_piso,
-            0.0,
-            screen_height / 2.0,
-            WHITE, // Color base
-            DrawTextureParams {
-                dest_size: Some(Vec2::new(screen_width, screen_height / 2.0)), // Escalar textura a la pared
-                ..Default::default()
-            }
-        );
+            draw_texture_ex(
+                &texture_techo,
+                0.0,
+                0.0,
+                WHITE, // Color base
+                DrawTextureParams {
+                    dest_size: Some(Vec2::new(screen_width, screen_height / 2.0)), // Escalar textura a la pared
+                    ..Default::default()
+                }
+            );
 
-        // Lanza rayos en un campo de visión
-        let num_rays = (screen_width * 3.0) as usize; // Aumenta el número de rayos
-        let max_depth = 18.0;
-        let line_width = screen_width / (num_rays as f32);
-        let mut texture = texture_pared.clone();
+            draw_texture_ex(
+                &texture_piso,
+                0.0,
+                screen_height / 2.0,
+                WHITE, // Color base
+                DrawTextureParams {
+                    dest_size: Some(Vec2::new(screen_width, screen_height / 2.0)), // Escalar textura a la pared
+                    ..Default::default()
+                }
+            );
 
-        let mut vector_render: Vec<RenderObjects> = Vec::new();
+            // Lanza rayos en un campo de visión
+            let num_rays = (screen_width * 3.0) as usize; // Aumenta el número de rayos
+            let max_depth = 18.0;
+            let line_width = screen_width / (num_rays as f32);
+            let mut texture = texture_pared.clone();
 
-        for i in 0..num_rays {
-            let ray_angle = player.angle - FOV / 2.0 + FOV * ((i as f32) / (num_rays as f32));
+            let mut vector_render: Vec<RenderObjects> = Vec::new();
 
-            // Dirección del rayo
-            let eye_x = ray_angle.cos();
-            let eye_y = ray_angle.sin();
+            for i in 0..num_rays {
+                let ray_angle = player.angle - FOV / 2.0 + FOV * ((i as f32) / (num_rays as f32));
 
-            // Posición inicial del rayo
-            let mut ray_x = player.x;
-            let mut ray_y = player.y;
+                // Dirección del rayo
+                let eye_x = ray_angle.cos();
+                let eye_y = ray_angle.sin();
 
-            // Cálculo del paso en el eje X y Y
-            let delta_dist_x = if eye_x != 0.0 { (1.0 / eye_x).abs() } else { f32::INFINITY };
-            let delta_dist_y = if eye_y != 0.0 { (1.0 / eye_y).abs() } else { f32::INFINITY };
+                // Posición inicial del rayo
+                let mut ray_x = player.x;
+                let mut ray_y = player.y;
 
-            // Determina el paso y la primera intersección
-            let mut step_x = 0;
-            let mut side_dist_x = if eye_x < 0.0 {
-                step_x = -1;
-                player.x.fract() * delta_dist_x
-            } else {
-                step_x = 1;
-                (1.0 - player.x.fract()) * delta_dist_x
-            };
+                // Cálculo del paso en el eje X y Y
+                let delta_dist_x = if eye_x != 0.0 { (1.0 / eye_x).abs() } else { f32::INFINITY };
+                let delta_dist_y = if eye_y != 0.0 { (1.0 / eye_y).abs() } else { f32::INFINITY };
 
-            let mut step_y = 0;
-            let mut side_dist_y = if eye_y < 0.0 {
-                step_y = -1;
-                player.y.fract() * delta_dist_y
-            } else {
-                step_y = 1;
-                (1.0 - player.y.fract()) * delta_dist_y
-            };
-
-            // Bucle DDA
-            let mut hit = false;
-            let mut hit_vertical = false;
-            let mut distance = 0.0;
-
-            while !hit && distance < max_depth {
-                // Avanza hacia la siguiente intersección
-                if side_dist_x < side_dist_y {
-                    ray_x += step_x as f32;
-                    distance = side_dist_x;
-                    side_dist_x += delta_dist_x;
-                    hit_vertical = true;
+                // Determina el paso y la primera intersección
+                let mut step_x = 0;
+                let mut side_dist_x = if eye_x < 0.0 {
+                    step_x = -1;
+                    player.x.fract() * delta_dist_x
                 } else {
-                    ray_y += step_y as f32;
-                    distance = side_dist_y;
-                    side_dist_y += delta_dist_y;
-                    hit_vertical = false;
+                    step_x = 1;
+                    (1.0 - player.x.fract()) * delta_dist_x
+                };
+
+                let mut step_y = 0;
+                let mut side_dist_y = if eye_y < 0.0 {
+                    step_y = -1;
+                    player.y.fract() * delta_dist_y
+                } else {
+                    step_y = 1;
+                    (1.0 - player.y.fract()) * delta_dist_y
+                };
+
+                // Bucle DDA
+                let mut hit = false;
+                let mut hit_vertical = false;
+                let mut distance = 0.0;
+
+                while !hit && distance < max_depth {
+                    // Avanza hacia la siguiente intersección
+                    if side_dist_x < side_dist_y {
+                        ray_x += step_x as f32;
+                        distance = side_dist_x;
+                        side_dist_x += delta_dist_x;
+                        hit_vertical = true;
+                    } else {
+                        ray_y += step_y as f32;
+                        distance = side_dist_y;
+                        side_dist_y += delta_dist_y;
+                        hit_vertical = false;
+                    }
+
+                    // Calcula la posición en el mapa
+                    let test_x = ray_x as usize;
+                    let test_y = ray_y as usize;
+
+                    // Verifica si hemos alcanzado una pared
+                    if test_x < MAP[0].len() && test_y < MAP.len() && MAP[test_y][test_x] > 0 {
+                        hit = true;
+
+                        texture = match MAP[test_y][test_x] {
+                            2 => texture_techo.clone(),
+                            _ => texture_pared.clone(),
+                        };
+                    }
                 }
 
-                // Calcula la posición en el mapa
-                let test_x = ray_x as usize;
-                let test_y = ray_y as usize;
-
-                // Verifica si hemos alcanzado una pared
-                if test_x < MAP[0].len() && test_y < MAP.len() && MAP[test_y][test_x] > 0 {
-                    hit = true;
-
-                    texture = match MAP[test_y][test_x] {
-                        2 => texture_techo.clone(),
-                        _ => texture_pared.clone(),
-                    };
-                }
-            }
-
-            if hit {
-                vector_render.push(
-                    RenderObjects::new(
-                        distance,
-                        true,
-                        Some(RenderWalls {
-                            distance,
-                            player: &player,
-                            ray_angle,
-                            eye_x,
-                            eye_y,
-                            hit_vertical,
-                            texture: texture.clone(),
-                            i,
-                            line_width,
-                            screen_height,
-                        }),
-                        None
-                    )
-                );
-            }
-        }
-
-        for entity in &entities {
-            let distance = calculate_distance(&player, entity);
-            println!("{}", distance);
-            // Ignorar entidades fuera del rango visible
-            if distance < max_depth {
-                let angle_to_entity = (entity.y - player.y).atan2(entity.x - player.x);
-                let angle_diff =
-                    ((angle_to_entity - player.angle + std::f32::consts::PI) %
-                        (2.0 * std::f32::consts::PI)) -
-                    std::f32::consts::PI;
-
-                // Si la entidad está dentro del FOV
-                if angle_diff.abs() < FOV / 1.8 {
-                    // 2.0
-                    let size = screen_height / distance; // Tamaño relativo al jugador
-                    let screen_x =
-                        screen_width / 2.0 + (angle_diff / (FOV / 2.0)) * (screen_width / 2.0);
+                if hit {
                     vector_render.push(
                         RenderObjects::new(
                             distance,
-                            false,
-                            None,
-                            Some(
-                                Box::new(RenderEntity {
-                                    distance,
-                                    texture: &entity.texture,
-                                    screen_x,
-                                    screen_y: screen_height / 2.0, // Ajustar según diseño
-                                    size,
-                                })
-                            )
+                            Some(RenderWalls {
+                                distance,
+                                player: &player,
+                                ray_angle,
+                                eye_x,
+                                eye_y,
+                                hit_vertical,
+                                texture: texture.clone(),
+                                i,
+                                line_width,
+                                screen_height,
+                            }),
+                            None
                         )
                     );
                 }
             }
-        }
 
-        vector_render.sort_by(|a, b| b.distance.partial_cmp(&a.distance).unwrap());
+            for entity in &entities {
+                let distance = calculate_distance(&player, entity);
+                // println!("{}", distance);
+                // Ignorar entidades fuera del rango visible
+                if distance < max_depth {
+                    let angle_to_entity = (entity.y - player.y).atan2(entity.x - player.x);
+                    let angle_diff =
+                        ((angle_to_entity - player.angle + std::f32::consts::PI) %
+                            (2.0 * std::f32::consts::PI)) -
+                        std::f32::consts::PI;
 
-        for obj in &vector_render {
-            if let Some(entity) = &obj.render_wall {
-                render(entity);
-            } else if let Some(entity) = &obj.render_entity {
-                render_entity(entity);
+                    // Si la entidad está dentro del FOV
+                    if angle_diff.abs() < FOV / 1.8 {
+                        // 2.0
+                        let size = screen_height / distance; // Tamaño relativo al jugador
+                        let screen_x =
+                            screen_width / 2.0 + (angle_diff / (FOV / 2.0)) * (screen_width / 2.0);
+                        vector_render.push(
+                            RenderObjects::new(
+                                distance,
+                                None,
+                                Some(
+                                    Box::new(RenderEntity {
+                                        distance,
+                                        texture: &entity.texture,
+                                        screen_x,
+                                        screen_y: screen_height / 2.0, // Ajustar según diseño
+                                        size,
+                                    })
+                                )
+                            )
+                        );
+                    }
+                }
             }
-        }
 
-        // Movement
-        if is_key_down(KeyCode::W) {
-            let new_x = player.x + player.angle.cos() * 0.1;
-            let new_y = player.y + player.angle.sin() * 0.1;
+            vector_render.sort_by(|a, b| b.distance.partial_cmp(&a.distance).unwrap());
 
-            // Verificar colisiones
-            if MAP[new_y as usize][new_x as usize] == 0 {
-                player.x = new_x;
-                player.y = new_y;
+            for obj in &vector_render {
+                if let Some(entity) = &obj.render_wall {
+                    render(entity);
+                } else if let Some(entity) = &obj.render_entity {
+                    render_entity(entity);
+                }
             }
-        }
-        if is_key_down(KeyCode::S) {
-            let new_x = player.x - player.angle.cos() * 0.1;
-            let new_y = player.y - player.angle.sin() * 0.1;
 
-            if MAP[new_y as usize][new_x as usize] == 0 {
-                player.x = new_x;
-                player.y = new_y;
+            // Movement
+            if is_key_down(KeyCode::W) {
+                let new_x = player.x + player.angle.cos() * 0.1;
+                let new_y = player.y + player.angle.sin() * 0.1;
+
+                // Verificar colisiones
+                if MAP[new_y as usize][new_x as usize] == 0 {
+                    player.x = new_x;
+                    player.y = new_y;
+                }
             }
-        }
-        // Manipulating Angle
-        if is_key_down(KeyCode::A) {
-            player.angle -= 0.05;
-        }
-        if is_key_down(KeyCode::D) {
-            player.angle += 0.05;
-        }
-        // Manipulating Angle with Mouse
-        let (mouse_x, _mouse_y) = mouse_position();
-        let delta_x = mouse_x - last_mouse_x;
-        player.angle += delta_x * 0.0025;
-        last_mouse_x = mouse_x;
+            if is_key_down(KeyCode::S) {
+                let new_x = player.x - player.angle.cos() * 0.1;
+                let new_y = player.y - player.angle.sin() * 0.1;
 
-        draw_text(&format!("FPS: {}", get_fps()), 10.0, 20.0, 30.0, BLACK);
-        draw_text(&format!("X: {}\n", player.x.round()), 10.0, 50.0, 30.0, BLACK);
-        draw_text(&format!("Y: {}", player.y.round()), 10.0, 80.0, 30.0, BLACK);
+                if MAP[new_y as usize][new_x as usize] == 0 {
+                    player.x = new_x;
+                    player.y = new_y;
+                }
+            }
+            // Manipulating Angle
+            if is_key_down(KeyCode::A) {
+                player.angle -= 0.05;
+            }
+            if is_key_down(KeyCode::D) {
+                player.angle += 0.05;
+            }
+            // Manipulating Angle with Mouse
+            let (mouse_x, _mouse_y) = mouse_position();
+            let delta_x = mouse_x - last_mouse_x;
+            player.angle += delta_x * 0.0025;
+            last_mouse_x = mouse_x;
+
+            draw_text(&format!("FPS: {}", get_fps()), 10.0, 20.0, 30.0, BLACK);
+            draw_text(&format!("X: {}\n", player.x.round()), 10.0, 50.0, 30.0, BLACK);
+            draw_text(&format!("Y: {}", player.y.round()), 10.0, 80.0, 30.0, BLACK);
+        }
 
         if is_key_down(KeyCode::Escape) {
-            exit(0)
+            exit(0);
         }
 
         next_frame().await;
